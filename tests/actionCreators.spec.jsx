b062af8cc3c9b4d3a3db3ca70531ec721ef9dc75
Aaron Norby <aaron.norby@gmail.com>
2016-01-03 10:59:40 -0800

    Add test for async action creator and fix async action creator

--- tests/actionCreators.spec.jsx
+++ tests/actionCreators.spec.jsx
@@ -1,5 +1,12 @@
-import { actionOne } from '../src/actions/index';
-import { ACTION_ONE } from '../src/actions/index';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { actionOne, performAsyncAction } from '../src/actions/index';
+import { ACTION_ONE, ASYNC_ACTION_START, ASYNC_ACTION_END } from '../src/actions/index';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
 
 describe('actionOne action creator', () => {
   it('should return a someProp with its action', () => {
@@ -13,6 +20,28 @@ describe('actionOne action creator', () => {
 
     expect(result.type).to.equal(expectedAction.type);
     expect(result.someProp).to.equal(expectedAction.someProp);
+  });
+});
+
+describe('async action creator', () => {
+  it('should start and end async action', (done) => {
+    // we are going to use the redux-mock-store utility to mock our entire store to
+    // test async actions. It runs assertion internally (using 'expect') against
+    // the series of actions we provide
+    
+    
+    const INITIAL_STATE = {};
+
+    // the series of actions we expect to be dispatched
+    const expectedActions = [
+      {type: ASYNC_ACTION_START},
+      {type: ASYNC_ACTION_END, returnedData: 'Hello from app!'}
+    ];
+
+    const store = mockStore(INITIAL_STATE, expectedActions, done);
+
+    // start it off: 
+    store.dispatch(performAsyncAction());
   });
 });
 
